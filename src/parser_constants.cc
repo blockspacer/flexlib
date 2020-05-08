3e580f757eec8539d9a714b5ddcb40a52832e150
derofim <derofim@example.com>
2020-05-08 17:11:46 +0300

    usage for parser_constants.cc

--- src/parser_constants.cc
+++ src/parser_constants.cc
@@ -2,8 +2,18 @@
 
 namespace cxxctp {
 
+// USAGE:
+// auto hasAnnotate = clang::ast_matchers::hasAttr( clang::attr::Annotate );
+// auto finderMatcher = clang::ast_matchers::decl( hasAnnotate ).bind( kAnnotateAttrName );
+// Finder_.addMatcher(finderMatcher, &Checker_);
 const char kAnnotateAttrName[] = "bind_gen";
 
+// USAGE:
+// // will be replaced with 1234
+// __attribute__((annotate("{gen};{executeCodeAndReplace};\
+// new llvm::Optional<std::string>{\"1234\"};")))
+// int SOME_UNIQUE_NAME2
+// ;
 const char kRequiredAnnotationPrefix[] = "{gen};";
 
 } // namespace cxxctp
