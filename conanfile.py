fa37895921092d61174adeab80a7b1c68f9142dd
CI_Build_Bot <cibuildbot@cibuildbot.com>
2020-07-02 21:09:36 +0300

    corrade v2020.06

--- conanfile.py
+++ conanfile.py
@@ -239,7 +239,7 @@ class flexlib_conan_project(conan_build_helper.CMakePackage):
 
       self.requires("basis/master@conan/stable")
 
-      self.requires("corrade/2019.10@magnum/stable")
+      self.requires("corrade/2020.06@magnum/stable")
 
       self.requires("type_safe/0.2@conan/stable")
 
