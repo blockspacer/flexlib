86c4290a72010cd24272788ad53e552712863794
user <user@email.ru>
2021-03-19 19:20:18 +0300

    UPD: conan_gtest/stable

--- conanfile.py
+++ conanfile.py
@@ -256,7 +256,7 @@ class flexlib_conan_project(conan_build_helper.CMakePackage):
 
       if self._is_tests_enabled():
           self.requires("catch2/[>=2.1.0]@bincrafters/stable")
-          self.requires("conan_gtest/release-1.10.0@conan/stable")
+          self.requires("conan_gtest/stable@conan/stable")
           self.requires("FakeIt/[>=2.0.4]@gasuketsu/stable")
 
       # TODO: support doctest
